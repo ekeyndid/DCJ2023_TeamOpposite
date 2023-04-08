b3f041fbd0643e0fb129b4531da554801d17759d
Keyndid Elmi <ekeyndid@gmail.com>
2023-04-07 20:45:09 -0400

    Dungeon Generator Complete

--- Assets/Scripts/RoomBehaviour.cs
+++ Assets/Scripts/RoomBehaviour.cs
@@ -8,21 +8,10 @@ public class RoomBehaviour : MonoBehaviour
     public GameObject[] walls; // 0 - up, 1 - down, 2 - right, 3 - left
     public GameObject[] doors;
 
-    public bool[] testStatus;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        UpdateRoom(testStatus);
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
-    void UpdateRoom(bool[] status)
+    public void UpdateRoom(bool[] status)
     {
         for(int i = 0; i < status.Length; i++)
         {
